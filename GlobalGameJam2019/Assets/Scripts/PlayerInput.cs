fee4c2b0502fde7927e01f0fe9bb503d65c581e0
Lars Kruik <Lars.Kruik@live.nl>
2019-01-25 22:00:52 +0100

    Added first player character itteration

---
+++ GlobalGameJam2019/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Player))]
+public class PlayerInput: MonoBehaviour
+{
+    private Player player;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        player.SetDirectionalInput(directionalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            player.OnJumpInputDown();
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            player.OnJumpInputUp();
+        }
+    }
+}
