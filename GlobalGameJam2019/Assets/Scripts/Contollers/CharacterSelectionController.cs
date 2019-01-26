c1c86bd41ee2bc34ac4e53325873aeda6be5d86e
Tim <timjlucassen@gmail.com>
2019-01-26 08:07:20 +0100

    Menu Flow
    Added a startScreen
    Added Access to Character slection through start screen
    Updated player prefab in Character selection
    Removed countdown from characterselection

---
+++ GlobalGameJam2019/Assets/Scripts/Contollers/CharacterSelectionController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelectionController : MonoBehaviour
+{
+
+    [SerializeField] private string StartupSceneName;
+    [SerializeField] private float RequiredHeldCancelTime;
+    [SerializeField] private float CurrentHeldBCancelTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(StartupSceneName == "")
+        {
+            Debug.Log("[WARNING] - [CharacterSelectionController] - StartupSceneName not assiged");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        ProcessExit();
+    }
+
+    private void ProcessExit()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Cancel"))
+        {
+            //Play Cancel Audio?
+        }
+
+        if(Input.GetKey(KeyCode.Escape) || Input.GetButton("Cancel"))
+        {
+            CurrentHeldBCancelTime += Time.deltaTime;
+            if(CurrentHeldBCancelTime >= RequiredHeldCancelTime)
+            {
+                SceneManager.LoadScene(StartupSceneName);
+            }
+        }
+        else
+        {
+            CurrentHeldBCancelTime = 0.0f;
+        }
+
+    }
+}
---
+++ GlobalGameJam2019/Assets/Scripts/Contollers/StartupController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartupController : MonoBehaviour
+{
+
+    [SerializeField] private AudioManager audioManager;
+    [SerializeField] private string CharacterSelectSceneName;
+    [SerializeField] private float StartedJuiceCountdown;
+    [SerializeField] private bool GameStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(CharacterSelectSceneName == "")
+        {
+            Debug.Log("[WARNING] - [StartupController] - Character selection scene not selected");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameStarted)
+        {
+            CheckToStartGame();
+        }
+        else
+        {
+            ProcessGameStarting();
+        }
+
+    }
+
+    void CheckToStartGame()
+    {
+        if (Input.GetKey(KeyCode.Return) || Input.GetButtonDown("Submit"))
+        {
+            GameStarted = true;
+            audioManager.PlaySound("ActionOkay");
+            Debug.Log("[StartupController] - Starting Juice countdown");
+        }
+    }
+
+    private void ProcessGameStarting()
+    {
+        StartedJuiceCountdown -= Time.deltaTime;
+        if (StartedJuiceCountdown <= 0.0f)
+        {
+            Debug.Log("[StartupController] - Loading Scene: " + CharacterSelectSceneName);
+            SceneManager.LoadScene(CharacterSelectSceneName);
+        }
+    }
+}
