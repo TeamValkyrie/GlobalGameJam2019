b8cedf33b09693d49b04b8ba5c440b99eb5706c7
Jasper Drescher <jasperdre@gmail.com>
2019-01-26 14:21:27 +0100

    Added images for UI buttons

---
+++ GlobalGameJam2019/Assets/Scripts/ScriptableObject/XboxButtonObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "XboxButtonData", menuName = "UI/Xbox/Button", order = 1)]
+public class XboxButtonObject : ScriptableObject
+{
+    public Color color = Color.white;
+    public string button;
+}
