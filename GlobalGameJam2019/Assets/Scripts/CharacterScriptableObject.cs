6e09a63e9a69bbcff48699f7c59caf2b3dcad89d
Jasper Drescher <jasperdre@gmail.com>
2019-01-26 18:12:15 +0100

    Added character previews

---
+++ GlobalGameJam2019/Assets/Scripts/CharacterScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "UI/Character", order = 1)]
+public class CharacterScriptableObject : ScriptableObject
+{
+    public Sprite preview;
+    public string name;
+}
--- GlobalGameJam2019/Assets/Scripts/Contollers/CharacterSelectionController.cs
+++ GlobalGameJam2019/Assets/Scripts/Contollers/CharacterSelectionController.cs
@@ -7,24 +7,30 @@ using UnityEngine.UI;
 public class CharacterSelectionController : MonoBehaviour
 {
     [System.Serializable]
-    struct PlayerUI
+    public struct PlayerUI
     {
         public List<GameObject> targets;
+        public Image preview;
+        public Text characterName;
         public Color color;
     }
+
+    [SerializeField] private List<CharacterScriptableObject> characters;
 
     [SerializeField] private string StartupSceneName;
     [SerializeField] private float RequiredHeldCancelTime;
     [SerializeField] private float CurrentHeldBCancelTime;
 
-    [SerializeField] private List<PlayerUI> PlayerInterfaces;
+    [SerializeField] private List<PlayerUI> playerInterfaces;
 
     [Header("Info")]
     public GameObject joinPanel;
     public GameObject startPanel;
     public GameObject startIndicator;
+    public Sprite unknownCharacter;
 
     private PlayerManager playerManager;
+    private List<int> playerCharacterIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -35,9 +41,14 @@ public class CharacterSelectionController : MonoBehaviour
         }
 
         playerManager = FindObjectOfType<PlayerManager>();
-
-        foreach (PlayerUI playerUI in PlayerInterfaces)
+        playerCharacterIndex = new List<int>();
+        
+        foreach (PlayerUI playerUI in playerInterfaces)
         {
+            playerUI.preview.sprite = unknownCharacter;
+            playerUI.characterName.text = "Unknown";
+            playerCharacterIndex.Add(0);
+
             foreach (GameObject targetUI in playerUI.targets)
             {
                 if (targetUI.GetComponent<Image>())
@@ -76,18 +87,32 @@ public class CharacterSelectionController : MonoBehaviour
 
         for (int i = 0; i < playerManager.GetConnectedPlayers(); i++)
         {
-            foreach (GameObject targetUI in PlayerInterfaces[i].targets)
+            foreach (GameObject targetUI in playerInterfaces[i].targets)
             {
                 if (targetUI.GetComponent<Image>())
                 {
-                    targetUI.GetComponent<Image>().color = PlayerInterfaces[i].color;
+                    targetUI.GetComponent<Image>().color = playerInterfaces[i].color;
                 }
 
                 if (targetUI.GetComponent<Text>())
                 {
-                    targetUI.GetComponent<Text>().color = PlayerInterfaces[i].color;
+                    targetUI.GetComponent<Text>().color = playerInterfaces[i].color;
                 }
             }
+
+            if (Input.GetAxis("Horizontal" + (i + 1)) > 0)
+            {
+                SelectNextCharacter(i);
+            }
+            else if (Input.GetAxis("Horizontal" + (i + 1)) < 0)
+            {
+                SelectPreviousCharacter(i);
+            }
+
+            Image preview = playerInterfaces[i].preview;
+            Text name = playerInterfaces[i].characterName;
+            preview.sprite = characters[playerCharacterIndex[i]].preview;
+            name.text = characters[playerCharacterIndex[i]].name;
         }
 
         if (playerManager.GetConnectedPlayers() > 0)
@@ -107,5 +132,45 @@ public class CharacterSelectionController : MonoBehaviour
                 startPanel.SetActive(false);
             }
         }
+    }
+
+    private void SelectNextCharacter(int playerIndex)
+    {
+        Image preview = playerInterfaces[playerIndex].preview;
+        Text name = playerInterfaces[playerIndex].characterName;
+
+        if (playerCharacterIndex[playerIndex] >= characters.Count - 1)
+        {
+            playerCharacterIndex[playerIndex] = 0;
+        }
+        else
+        {
+            playerCharacterIndex[playerIndex]++;
+        }
+
+        preview.sprite = characters[playerCharacterIndex[playerIndex]].preview;
+        name.text = characters[playerCharacterIndex[playerIndex]].name;
+
+        Debug.Log("Player " + playerIndex + " selected the next character");
+    }
+
+    private void SelectPreviousCharacter(int playerIndex)
+    {
+        Image preview = playerInterfaces[playerIndex].preview;
+        Text name = playerInterfaces[playerIndex].characterName;
+
+        if (playerCharacterIndex[playerIndex] <= 0)
+        {
+            playerCharacterIndex[playerIndex] = characters.Count - 1;
+        }
+        else
+        {
+            playerCharacterIndex[playerIndex]--;
+        }
+
+        preview.sprite = characters[playerCharacterIndex[playerIndex]].preview;
+        name.text = characters[playerCharacterIndex[playerIndex]].name;
+
+        Debug.Log("Player " + playerIndex + " selected the previous character");
     }
 }
